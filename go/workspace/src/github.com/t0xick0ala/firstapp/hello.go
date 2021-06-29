f37e4532a795b7d453b68b3de213ceccbc5deef7
t0xick0ala <toxicuskoala@gmail.com>
2021-06-30 07:00:21 +1000

    Shadowing

--- go/workspace/src/github.com/t0xick0ala/firstapp/hello.go
+++ go/workspace/src/github.com/t0xick0ala/firstapp/hello.go
@@ -2,25 +2,27 @@ package main
 
 import "fmt"
 
-var (
-	actorname  string = "Elisabeth Sladen"
-	companion  string = "Sarah Jane Smith"
-	doctorName int    = 3
-	season     int    = 11
-)
+// var (
+// 	actorname  string = "Elisabeth Sladen"
+// 	companion  string = "Sarah Jane Smith"
+// 	doctorName int    = 3
+// 	season     int    = 11
+// )
 
-var (
-	counter int = 0
-)
+// var (
+// 	counter int = 0
+// )
+var i int = 27
 
 func main() {
-
-	var i float32 = 42
-	var j int
-	j = 27
-	k := 99
-	//fmt.Printf("hello, world\n")
-	fmt.Println(i, j, k)
-	fmt.Printf("%v, %T\n", j, i) // printf: print format, %v - value, %T - type
-	//fmt.Printf("%v, %T", l, l)
+	fmt.Println(i)
+	var i int = 42
+	fmt.Println(i)
+	// var j int
+	// j = 27
+	// k := 99
+	// //fmt.Printf("hello, world\n")
+	// fmt.Println(i, j, k)
+	// fmt.Printf("%v, %T\n", j, i) // printf: print format, %v - value, %T - type
+	// //fmt.Printf("%v, %T", l, l)
 }
