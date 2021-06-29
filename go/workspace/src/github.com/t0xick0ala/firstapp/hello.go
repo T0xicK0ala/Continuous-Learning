3ef4f9e18b370abb3d5c1d72a7d038657b41c3bf
t0xick0ala <toxicuskoala@gmail.com>
2021-06-30 06:52:42 +1000

    variable 1

--- go/workspace/src/github.com/t0xick0ala/firstapp/hello.go
+++ go/workspace/src/github.com/t0xick0ala/firstapp/hello.go
@@ -1,6 +1,17 @@
 package main
+
 import "fmt"
 
+var l int = 55 // global level variable
+
 func main() {
-    fmt.Printf("hello, world\n")
+
+	var i float32 = 42
+	var j int
+	j = 27
+	k := 99
+	//fmt.Printf("hello, world\n")
+	fmt.Println(i, j, k)
+	fmt.Printf("%v, %T\n", j, i) // printf: print format, %v - value, %T - type
+	fmt.Printf("%v, %T", l, l)
 }
