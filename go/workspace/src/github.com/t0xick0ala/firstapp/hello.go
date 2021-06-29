9b385b11795aaca2339366815a6bc0ae9317678e
t0xick0ala <toxicuskoala@gmail.com>
2021-06-30 08:28:58 +1000

    type conversion

--- go/workspace/src/github.com/t0xick0ala/firstapp/hello.go
+++ go/workspace/src/github.com/t0xick0ala/firstapp/hello.go
@@ -1,28 +1,19 @@
 package main
 
-import "fmt"
-
-// var (
-// 	actorname  string = "Elisabeth Sladen"
-// 	companion  string = "Sarah Jane Smith"
-// 	doctorName int    = 3
-// 	season     int    = 11
-// )
-
-// var (
-// 	counter int = 0
-// )
-var i int = 27
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println(i)
 	var i int = 42
-	fmt.Println(i)
-	// var j int
-	// j = 27
-	// k := 99
-	// //fmt.Printf("hello, world\n")
-	// fmt.Println(i, j, k)
-	// fmt.Printf("%v, %T\n", j, i) // printf: print format, %v - value, %T - type
-	// //fmt.Printf("%v, %T", l, l)
+	fmt.Printf("%v, %T\n", i, i)
+
+	var j float32
+	j = float32(i)
+	fmt.Printf("%v, %T\n", j, j)
+
+	var k string
+	k = strconv.Itoa(i)
+	fmt.Printf("%v, %T\n", k, k)
 }
